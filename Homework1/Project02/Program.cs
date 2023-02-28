2717e7d3a37bd0419f37a82c3be8a60c6a5e4c11
Kristina <misskristsi@yandex.ru>
2023-03-01 04:18:05 +0600

    Project02

---
+++ Homework1/Project02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает 
+// два числа и выдает, какое число большее, а какое меньшее.
+
+Console.Write("Напишите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Напишите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if (a > b) 
+{
+    max = a;
+    Console.WriteLine($"Больше число {max}");
+}
+else 
+{
+    max = b;
+    Console.WriteLine($"Больше число {max}");
+}
