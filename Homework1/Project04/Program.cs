6d84aa8293b8061019fa47e9404a06fb0a7ae9e7
Kristina <misskristsi@yandex.ru>
2023-03-01 04:34:40 +0600

    Project04

---
+++ Homework1/Project04/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход 
+// три числа и выдает максимальное из этих чисел.
+
+Console.Write("Напишите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Напишите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Напишите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write($"Наибольшее число ");
+Console.WriteLine(max);
+
