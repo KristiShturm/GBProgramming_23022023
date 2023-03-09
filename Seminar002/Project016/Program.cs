3ec0f4a16aa75deac460745bc50d9b318bac4d82
Kristina <misskristsi@yandex.ru>
2023-03-10 02:11:27 +0600

    add project016

---
+++ Seminar002/Project016/Program.cs
@@ -0,0 +1,52 @@
+﻿/*Напишите программу, которая принимает на вход два числа и 
+проверяет, является ли одно число квадратом другого.
+5, 25  ->  да
+-4, 16  ->  да
+25, 5  ->  да
+8,9  ->  нет
+*/
+Console.Write("Напишите первое число: ");
+int first = int.Parse(Console.ReadLine());
+
+Console.Write("Напишите второе число: ");
+int second = int.Parse(Console.ReadLine());
+
+/*if(first > second)
+{
+    if(second*second == first)
+    {
+        Console.Write("Да");
+    }
+    else
+    {
+        Console.Write("Нет");
+    }
+}
+else
+{
+    if(first*first == second)
+    {
+        Console.Write("Да");
+    }
+    else
+    {
+        Console.Write("Нет");
+    }
+}
+*/
+if(first > second)
+{
+    second *= second;
+}
+else
+{
+    first *= first;
+}
+if(first == second)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
