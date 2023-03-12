f7a3ffcb3a06560f60a086a86318a5928ed13900
Kristina <misskristsi@yandex.ru>
2023-03-12 08:00:00 +0600

    add project022

---
+++ Seminar003/Project022/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая принимает на вход 
+число (N) и выдаёт таблицу квадратов чисел 
+от 1 до N.
+
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+
+System.Console.Write("Напишите число: ");
+int number = int.Parse(Console.ReadLine());
+quad(number);
+void quad (int x)
+{
+for (int i = 1; i <= x; i++)
+{
+    if (i == x)
+    {
+        System.Console.WriteLine(i * i);
+        break;
+    }
+    System.Console.Write(i * i + ", ");
+    
+}
+}
+
+
+
