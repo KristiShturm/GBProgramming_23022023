08b1c449c746b0be050bfd290a1f41f98bc8f8c2
Kristina <misskristsi@yandex.ru>
2023-03-12 06:44:05 +0600

    add project018

---
+++ Seminar003/Project018/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+Console.Write("Напишите номер четверти: ");
+int num = int.Parse(Console.ReadLine());
+
+switch(num)
+{
+    case 1:
+         Console.Write("x>0, y>o");
+         break;
+    case 2:
+         Console.Write("x<0, y>o");
+         break;
+    case 3:
+         Console.Write("x<0, y<o");
+         break;
+    case 4:
+         Console.Write("x>0, y<o");
+         break;
+    default: 
+         Console.Write("Нет такой четверти!");
+         break;
+}
+
