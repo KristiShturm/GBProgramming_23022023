3b8aaa8f752ee773a33c3d7d1205b3fcf974e2bb
Kristina <misskristsi@yandex.ru>
2023-04-10 22:04:17 +0600

    Add Project62

---
+++ Homework8/Project62/Program.cs
@@ -0,0 +1,77 @@
+﻿/* Напишите программу, которая заполнит спирально массив 4 на 4*/
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void PrintArray(int[,] tableArray)
+{
+    Console.WriteLine();
+    for (int i = 0; i < tableArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < tableArray.GetLength(1); j++)
+        {
+            Console.Write($"{tableArray[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GenerateArray(int numberRows, int numberColumns)
+{
+    int nextNumber = 1;
+    int[,] array = new int[numberRows, numberColumns];
+
+    int roundNumbers = 0;
+   
+    while (nextNumber <= array.GetLength(0) * array.GetLength(1))
+    {
+        int i= roundNumbers;
+        int j = roundNumbers;
+
+        for (j = roundNumbers; j < numberColumns; j++)
+        {
+            array[i, j] = nextNumber;
+            nextNumber++;
+        }
+        
+        if (nextNumber > array.GetLength(0) * array.GetLength(1))
+        {
+            break;
+        }
+        j = numberColumns-1;
+        for (i = roundNumbers+1; i < numberRows; i++)
+        {
+            array[i, j] = nextNumber;
+            nextNumber++;
+        }
+        numberColumns--;
+       
+        i = numberRows-1;
+        for (j = numberColumns-1; j >= roundNumbers ; j--)
+        {
+            array[i, j] = nextNumber;
+            nextNumber++;
+        }
+        numberRows--;
+
+        j = roundNumbers;
+        roundNumbers++;
+        for (i = numberRows - 1; i >= roundNumbers; i--)
+        {
+            array[i, j] = nextNumber;
+            nextNumber++;
+        }
+    }
+
+    return array;
+}
+
+int numberRows = Prompt("Введите значение количества строк массива: ");
+int numberColumns = Prompt("Введите значение количества столбцов массива: ");
+
+int[,] matrix = GenerateArray(numberRows, numberColumns);
+PrintArray(matrix);
