3a103ccc6caff00e959124498e14366e71c61cbe
Kristina <misskristsi@yandex.ru>
2023-04-13 22:09:13 +0600

    Add Project01

---
+++ Homework9/Project01/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задайте значение N. Напишите программу, которая выведет все четные натуральные числа 
+в промежутке от m до N. Выполнить с помощью рекурсии.
+*/
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+(int, int) CheckInput(int M, int N)
+{
+    M = Math.Abs(M);
+    N = Math.Abs(N);
+
+    if (N < M)
+    {
+        int temp = M;
+        M = N;
+        N = temp;
+    }
+    return (M, N);
+}
+void GetEvenNumbers(int M, int N)
+{
+
+    if (M == N)
+    {
+        if ((M % 2) == 0)
+        {
+            Console.Write($"{M}");
+        }
+    }
+    else
+    {
+        if ((M % 2) == 0)
+        {
+            Console.Write($"{M}, ");
+        }
+        M++;
+        GetEvenNumbers(M, N);
+    }
+}
+
+int M = Prompt("Введите число M: ");
+int N = Prompt("Введите число N: ");
+
+(M, N) = CheckInput(M, N);
+
+Console.WriteLine($"Все четные числа из промежутка от {M} до {N} => ");
+GetEvenNumbers(M, N);
